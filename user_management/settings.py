08d6dfc6b7471d0eaf2d39425d4896a28e8724b8
Nathan Ngo <86464633+DatacollectorVN@users.noreply.github.com>
2023-07-29 13:19:21 +0700

    add host

--- user_management/settings.py
+++ user_management/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "bmetapp.azurewebsites.net"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "bmewebdev.azurewebsites.net"]
 
 
 # Application definition
